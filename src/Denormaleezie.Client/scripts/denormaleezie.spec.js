bca9c056eed39cb351c51ba4d56a05eb9f981c69
bofirial <james@jschafer.net>
2016-02-24 09:07:36 -0500

    Initial Jasmine Unit Tests

--- src/Denormaleezie.Client/scripts/denormaleezie.spec.js
+++ src/Denormaleezie.Client/scripts/denormaleezie.spec.js
@@ -1,1 +1,13 @@
-
+/// <reference path="../assets/jasmine.d.ts" />
+/// <reference path="denormaleezie.ts" />
+describe('Denormaleezie', function () {
+    it('should include a namespace', function () {
+        expect(denormaleezie).not.toBeUndefined();
+        expect(denormaleezie).not.toBeNull();
+    });
+    it('should include a denormalize function', function () {
+        expect(denormaleezie.denormalize).not.toBeUndefined();
+        expect(denormaleezie.denormalize).not.toBeNull();
+        expect(typeof (denormaleezie.denormalize)).toEqual("function");
+    });
+});
--- src/Denormaleezie.Client/scripts/denormaleezie.spec.ts
+++ src/Denormaleezie.Client/scripts/denormaleezie.spec.ts
@@ -1,1 +1,17 @@
-﻿
+﻿/// <reference path="../assets/jasmine.d.ts" />
+/// <reference path="denormaleezie.ts" />
+
+describe('Denormaleezie', () => {
+
+    it('should include a namespace', () => {
+        expect(denormaleezie).not.toBeUndefined();
+        expect(denormaleezie).not.toBeNull();
+    });
+
+    it('should include a denormalize function', () => {
+        expect(denormaleezie.denormalize).not.toBeUndefined();
+        expect(denormaleezie.denormalize).not.toBeNull();
+
+        expect(typeof(denormaleezie.denormalize)).toEqual("function");
+    });
+});
