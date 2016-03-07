a1bde65c3da772938d78cc5f960a778f8945e8d9
James Schafer <james@jschafer.net>
2016-03-07 11:03:52 -0500

    Refactoring NormalizedData and NormalizedStructure functions to their own Managers.  Also added a ReflectionHelper.  Rebuilt Unit Tests for Normalizer and ReflectionHelper.

---
+++ src/Normaleezie/NormalizedStructure/NormalizedStructureManager.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Normaleezie.NormalizedStructure
+{
+    internal class NormalizedStructureManager
+    {
+        internal virtual List<List<object>> CreateNormalizedStructureList<T>(List<T> denormalizedList
+            , List<List<object>> normalizedDataList)
+        {
+            if (null == denormalizedList)
+            {
+                throw new ArgumentException(nameof(denormalizedList) + " must not be null.", nameof(denormalizedList));
+            }
+
+            if (null == normalizedDataList)
+            {
+                throw new ArgumentException(nameof(normalizedDataList) + " must not be null.", nameof(normalizedDataList));
+            }
+
+            return denormalizedList.Select(denormalizedItem => CreateNormalizedStructureItem(denormalizedItem, normalizedDataList)).ToList();
+        }
+        internal virtual List<object> CreateNormalizedStructureItem(object denormalizedItem
+    , List<List<object>> normalizedDataList)
+        {
+            if (null == denormalizedItem)
+            {
+                throw new ArgumentException(nameof(denormalizedItem) + " must not be null.", nameof(denormalizedItem));
+            }
+
+            if (null == normalizedDataList)
+            {
+                throw new ArgumentException(nameof(normalizedDataList) + " must not be null.", nameof(normalizedDataList));
+            }
+
+            return normalizedDataList.Select(normalizedPropertyData => GetNormalizedItemPropertyObject(denormalizedItem, normalizedPropertyData)).ToList();
+        }
+
+        internal virtual object GetNormalizedItemPropertyObject(object denormalizedItem, List<object> normalizedPropertyData)
+        {
+            if (null == denormalizedItem)
+            {
+                throw new ArgumentException(nameof(denormalizedItem) + " must not be null.", nameof(denormalizedItem));
+            }
+
+            if (null == normalizedPropertyData || !normalizedPropertyData.Any())
+            {
+                throw new ArgumentException(nameof(normalizedPropertyData) + " must not be null.", nameof(normalizedPropertyData));
+            }
+
+            string propertyName = (string)normalizedPropertyData[0];
+
+            int index = propertyName.IndexOfAny(new char[] { '.', '~' });
+
+            if (index > -1)
+            {
+                if (propertyName[index] == '.')
+                {
+                    return GetNormalizedItemPropertyObjectForComplexProperty(denormalizedItem, normalizedPropertyData, propertyName);
+                }
+
+                return GetNormalizedItemPropertyObjectForListProperty(denormalizedItem, normalizedPropertyData, propertyName);
+            }
+
+            return GetNormalizedItemPropertyObjectForSimpleProperty(denormalizedItem, normalizedPropertyData, propertyName);
+        }
+
+        internal virtual object GetNormalizedItemPropertyObjectForSimpleProperty(object denormalizedItem,
+    List<object> normalizedPropertyData, string propertyName)
+        {
+            if (null == denormalizedItem)
+            {
+                throw new ArgumentException(nameof(denormalizedItem) + " must not be null.", nameof(denormalizedItem));
+            }
+
+            if (null == normalizedPropertyData)
+            {
+                throw new ArgumentException(nameof(normalizedPropertyData) + " must not be null.", nameof(normalizedPropertyData));
+            }
+
+            if (string.IsNullOrEmpty(propertyName))
+            {
+                throw new ArgumentException(nameof(propertyName) + " must not be null.", nameof(propertyName));
+            }
+
+            PropertyInfo propInfo = denormalizedItem.GetType().GetProperty(propertyName);
+            object val = propInfo.GetValue(denormalizedItem, null);
+
+            if (1 == normalizedPropertyData.Count)
+            {
+                return val;
+            }
+
+            int position = normalizedPropertyData.IndexOf(val);
+
+            if (-1 == position)
+            {
+                throw new InvalidOperationException(val.ToString() + " is missing from the denormalized data list.");
+            }
+
+            return position;
+        }
+
+        internal virtual object GetNormalizedItemPropertyObjectForListProperty(object denormalizedItem,
+            List<object> normalizedPropertyData, string propertyName)
+        {
+            if (null == denormalizedItem)
+            {
+                throw new ArgumentException(nameof(denormalizedItem) + " must not be null.", nameof(denormalizedItem));
+            }
+
+            if (null == normalizedPropertyData)
+            {
+                throw new ArgumentException(nameof(normalizedPropertyData) + " must not be null.", nameof(normalizedPropertyData));
+            }
+
+            if (string.IsNullOrEmpty(propertyName))
+            {
+                throw new ArgumentException(nameof(propertyName) + " must not be null.", nameof(propertyName));
+            }
+
+            string[] propertyNameParts = propertyName.Split('~');
+
+            object list = denormalizedItem;
+
+            if (!string.IsNullOrEmpty(propertyNameParts[0]))
+            {
+                PropertyInfo propInfo = denormalizedItem.GetType().GetProperty(propertyNameParts[0]);
+                list = propInfo.GetValue(denormalizedItem, null);
+            }
+
+            List<List<object>> normalizedItemsPropertyObjectList = new List<List<object>>();
+
+            foreach (var val in (IEnumerable)list)
+            {
+                var normalizedItemPropertyObject = new List<object>();
+
+                foreach (var normalizedSubPropertyData in normalizedPropertyData.Skip(1))
+                {
+                    normalizedItemPropertyObject.Add(GetNormalizedItemPropertyObject(val, (List<object>)normalizedSubPropertyData));
+                }
+
+                normalizedItemsPropertyObjectList.Add(normalizedItemPropertyObject);
+            }
+
+            return normalizedItemsPropertyObjectList;
+        }
+
+        internal virtual object GetNormalizedItemPropertyObjectForComplexProperty(object denormalizedItem,
+            List<object> normalizedPropertyData, string propertyName)
+        {
+            if (null == denormalizedItem)
+            {
+                throw new ArgumentException(nameof(denormalizedItem) + " must not be null.", nameof(denormalizedItem));
+            }
+
+            if (null == normalizedPropertyData)
+            {
+                throw new ArgumentException(nameof(normalizedPropertyData) + " must not be null.", nameof(normalizedPropertyData));
+            }
+
+            if (string.IsNullOrEmpty(propertyName))
+            {
+                throw new ArgumentException(nameof(propertyName) + " must not be null.", nameof(propertyName));
+            }
+
+            string[] propertyNameParts = propertyName.Split('.');
+
+            object val = denormalizedItem;
+
+            if (!string.IsNullOrEmpty(propertyNameParts[0]))
+            {
+                PropertyInfo propInfo = denormalizedItem.GetType().GetProperty(propertyNameParts[0]);
+                val = propInfo.GetValue(denormalizedItem, null);
+            }
+
+            List<object> normalizedItemPropertyObject = new List<object>();
+
+            foreach (var normalizedSubPropertyData in normalizedPropertyData.Skip(1))
+            {
+                normalizedItemPropertyObject.Add(GetNormalizedItemPropertyObject(val, (List<object>)normalizedSubPropertyData));
+            }
+
+            return normalizedItemPropertyObject;
+
+            //string[] propertyNameParts = propertyName.Split('.');
+            //string subPropName = string.Join(".", propertyNameParts.Skip(1));
+
+            //PropertyInfo propInfo = denormalizedItem.GetType().GetProperty(propertyNameParts[0]);
+            //object val = propInfo.GetValue(denormalizedItem, null);
+
+            //List<object> subPropertyNormalizedPropertyData = new List<object>() { subPropName };
+
+            //subPropertyNormalizedPropertyData.AddRange(normalizedPropertyData.Skip(1));
+
+            //return GetNormalizedItemPropertyObject(val, subPropertyNormalizedPropertyData);
+        }
+    }
+}
---
+++ test/Normaleezie.Tests/Helpers/ReflectionHelper_UnitTests.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FakeItEasy;
+using Normaleezie;
+using Normaleezie.Helpers;
+using Normaleezie.Tests.Test_Classes;
+using Xunit;
+// ReSharper disable InconsistentNaming
+// ReSharper disable CheckNamespace
+
+namespace nEZ.Unit.Helpers
+{
+    #region ConvertList
+    public class When_Calling_ConvertList_With_A_Null_Object
+    {
+        private readonly ReflectionHelper reflectionHelper;
+
+        public When_Calling_ConvertList_With_A_Null_Object()
+        {
+            reflectionHelper = new ReflectionHelper();
+        }
+
+        [Fact]
+        public void It_Should_Throw_An_ArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => reflectionHelper.ConvertList<object>(null));
+        }
+    }
+
+    public class When_Calling_ConvertList
+    {
+        private readonly List<Animal> returnedAnimals;
+
+        public When_Calling_ConvertList()
+        {
+            ReflectionHelper reflectionHelper = new ReflectionHelper();
+
+            List<object> animals = new List<object>()
+            {
+                new Animal() {Name = "Reggie"},
+                new Animal() {Name = "Ronnie"},
+                new Animal() {Name = "Ralphie"}
+            };
+
+            returnedAnimals = reflectionHelper.ConvertList<Animal>(animals);
+        }
+
+        [Fact]
+        public void It_Should_Return_A_List_Of_The_Generic_Type()
+        {
+            Assert.IsType(typeof(List<Animal>), returnedAnimals);
+        }
+    }
+
+    public class When_Calling_ConvertList_With_A_Mixed_List
+    {
+        private readonly ReflectionHelper reflectionHelper;
+        private readonly List<object> objects;
+
+        public When_Calling_ConvertList_With_A_Mixed_List()
+        {
+            reflectionHelper = new ReflectionHelper();
+
+            objects = new List<object>()
+            {
+                new Animal() {Name = "Reggie"},
+                new Person() {Name = "Ronnie"},
+                new Book() {Title = "Ralphie"}
+            };
+        }
+
+        [Fact]
+        public void It_Should_Throw_An_InvalidCastException()
+        {
+            Assert.Throws<InvalidCastException>(() => reflectionHelper.ConvertList<Animal>(objects));
+        }
+    }
+
+    #endregion
+
+    #region IsSimpleType
+    public class When_Calling_IsSimpleType_With_A_Null_Type
+    {
+        private readonly ReflectionHelper reflectionHelper;
+
+        public When_Calling_IsSimpleType_With_A_Null_Type()
+        {
+            reflectionHelper = new ReflectionHelper();
+        }
+
+        [Fact]
+        public void It_Should_Throw_An_Exception()
+        {
+            Assert.Throws(typeof(ArgumentException), () => reflectionHelper.IsSimpleType(null));
+        }
+    }
+
+    public class When_Calling_IsSimpleType
+    {
+        private readonly ReflectionHelper reflectionHelper;
+
+        public When_Calling_IsSimpleType()
+        {
+            reflectionHelper = new ReflectionHelper();
+        }
+
+        [Theory]
+        [InlineData(typeof(int))]
+        [InlineData(typeof(string))]
+        [InlineData(typeof(DateTime))]
+        [InlineData(typeof(float))]
+        [InlineData(typeof(byte))]
+        [InlineData(typeof(char))]
+        [InlineData(typeof(decimal))]
+        public void With_A_Simple_Type_It_Should_Return_True(Type type)
+        {
+            bool isSimpleType = reflectionHelper.IsSimpleType(type);
+
+            Assert.True(isSimpleType);
+        }
+
+        [Theory]
+        [InlineData(typeof(Book))]
+        [InlineData(typeof(List<string>))]
+        [InlineData(typeof(Tuple<string>))]
+        [InlineData(typeof(object))]
+        public void With_A_Complex_Type_It_Should_Return_False(Type type)
+        {
+            bool isSimpleType = reflectionHelper.IsSimpleType(type);
+            
+            Assert.False(isSimpleType);
+        }
+    }
+    #endregion
+
+}
