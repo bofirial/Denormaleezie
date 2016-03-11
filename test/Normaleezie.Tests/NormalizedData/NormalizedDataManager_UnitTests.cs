26f60734fe2da97688606f98e2361b0dc57c72f7
James Schafer <james@jschafer.net>
2016-03-11 09:36:36 -0500

    Completed Unit Testing for NormalizedDataManager

--- test/Normaleezie.Tests/NormalizedData/NormalizedDataManager_UnitTests.cs
+++ test/Normaleezie.Tests/NormalizedData/NormalizedDataManager_UnitTests.cs
@@ -256,13 +256,36 @@ namespace Unit_NormalizedDataManager
 
             List<Animal> denormalizedData = new List<Animal>()
             {
-                
+                new Animal() {Age = 2, AnimalId = 1, Name = "Fluffy", Type = "Bird"}
             };
 
             List<string> previousDataNames = new List<string>();
             string dataName = "soliloquy";
 
             List<List<object>> normalizedData = normalizedDataManager.CreateNormalizedDataForListOfComplexType(denormalizedData, previousDataNames, dataName);
+
+            A.CallTo(() => normalizedDataManager.CallCreateNormalizedDataGenerically(A<List<object>>.Ignored, A<List<string>>.Ignored, A<string>.Ignored, A<Type>.Ignored))
+                .MustHaveHappened(Repeated.Exactly.Times(4));
+
+            A.CallTo(() => normalizedDataManager.CallCreateNormalizedDataGenerically(A<List<object>>.Ignored, A<List<string>>.Ignored, A<string>.Ignored, A<Type>.Ignored))
+                .WhenArgumentsMatch(args => (string)args[2] == "AnimalId" && (int)(((List<object>)args[0])[0]) == 1 && args[1] == previousDataNames )
+                .MustHaveHappened();
+
+            A.CallTo(() => normalizedDataManager.CallCreateNormalizedDataGenerically(A<List<object>>.Ignored, A<List<string>>.Ignored, A<string>.Ignored, A<Type>.Ignored))
+                .WhenArgumentsMatch(args => (string)args[2] == "Age" && (int)(((List<object>)args[0])[0]) == 2 && args[1] == previousDataNames)
+                .MustHaveHappened();
+
+            A.CallTo(() => normalizedDataManager.CallCreateNormalizedDataGenerically(A<List<object>>.Ignored, A<List<string>>.Ignored, A<string>.Ignored, A<Type>.Ignored))
+                .WhenArgumentsMatch(args => (string)args[2] == "Name" && (string)(((List<object>)args[0])[0]) == "Fluffy" && args[1] == previousDataNames)
+                .MustHaveHappened();
+
+            A.CallTo(() => normalizedDataManager.CallCreateNormalizedDataGenerically(A<List<object>>.Ignored, A<List<string>>.Ignored, A<string>.Ignored, A<Type>.Ignored))
+                .WhenArgumentsMatch(args => (string)args[2] == "Type" && (string)(((List<object>)args[0])[0]) == "Bird" && args[1] == previousDataNames)
+                .MustHaveHappened();
+
+            A.CallTo(() => normalizedDataManager.FormatNormalizedDataForListOfComplexType(A<string>.Ignored, A<List<List<object>>>.Ignored))
+                .WhenArgumentsMatch(args => (string) args[0] == dataName)
+                .MustHaveHappened(Repeated.Exactly.Once);
 
             Assert.Equal(formatNormalizedDataForListOfComplexTypeReturn, normalizedData);
         }
@@ -273,10 +296,139 @@ namespace Unit_NormalizedDataManager
 
     #region FormatNormalizedDataForListOfComplexType
 
+    public class FormatNormalizedDataForListOfComplexType
+    {
+        private readonly NormalizedDataManager normalizedDataManager;
+
+        public FormatNormalizedDataForListOfComplexType()
+        {
+            normalizedDataManager = A.Fake<NormalizedDataManager>();
+
+            A.CallTo(() => normalizedDataManager.FormatNormalizedDataForListOfComplexType(A<string>.Ignored, A< List<List<object>>>.Ignored))
+                .CallsBaseMethod();
+        }
+
+        [Fact]
+        public void Should_Return_The_NormalizedDataByProperty_When_DataName_Is_Null()
+        {
+            string dataName = null;
+            List<List<object>> normalizedDataByProperty = new List<List<object>>();
+
+            List<List<object>> normalizedData = normalizedDataManager.FormatNormalizedDataForListOfComplexType(dataName, normalizedDataByProperty);
+
+            Assert.Equal(normalizedDataByProperty, normalizedData);
+        }
+
+        [Fact]
+        public void Should_Return_The_NormalizedDataByProperty_When_DataName_Is_Empty()
+        {
+            string dataName = string.Empty;
+            List<List<object>> normalizedDataByProperty = new List<List<object>>();
+
+            List<List<object>> normalizedData = normalizedDataManager.FormatNormalizedDataForListOfComplexType(dataName, normalizedDataByProperty);
+
+            Assert.Equal(normalizedDataByProperty, normalizedData);
+        }
+
+        [Fact]
+        public void Should_Return_The_NormalizedData_With_The_DataName_When_DataName_Is_Not_Null_Or_Empty()
+        {
+            string dataName = "Desperado";
+            List<List<object>> normalizedDataByProperty = new List<List<object>>()
+            {
+                new List<object>() { "1st", 201 },
+                new List<object>() { "2nd", 1502 }
+            };
+
+            List<List<object>> normalizedData = normalizedDataManager.FormatNormalizedDataForListOfComplexType(dataName, normalizedDataByProperty);
+
+            Assert.Equal("Desperado.", normalizedData[0][0]);
+            Assert.Equal(normalizedDataByProperty[0], normalizedData[0][1]);
+            Assert.Equal(normalizedDataByProperty[1], normalizedData[0][2]);
+        }
+
+    }
+
     #endregion
 
     #region CreateNormalizedDataForListOfIEnumerableType
 
+    public class CreateNormalizedDataForListOfIEnumerableType
+    {
+        private readonly NormalizedDataManager normalizedDataManager;
+
+        public CreateNormalizedDataForListOfIEnumerableType()
+        {
+            normalizedDataManager = A.Fake<NormalizedDataManager>();
+
+            A.CallTo(() => normalizedDataManager.CreateNormalizedDataForListOfIEnumerableType(A<List<List<Animal>>>.Ignored, A<List<string>>.Ignored, A<string>.Ignored))
+                .CallsBaseMethod();
+        }
+
+        [Fact]
+        public void Should_Throw_An_ArgumentNullException_When_The_DenormalizedList_Is_Null()
+        {
+            Assert.Throws<ArgumentNullException>(() => normalizedDataManager.CreateNormalizedDataForListOfIEnumerableType<List<Animal>>(null, new List<string>(), string.Empty));
+        }
+
+        [Fact]
+        public void Should_Call_CallCreateNormalizedDataGenerically_With_The_Data_In_The_List()
+        {
+            List<List<object>> callCreateNormalizedDataGenericallyReturn = new List<List<object>>();
+
+            A.CallTo(() => normalizedDataManager.CallCreateNormalizedDataGenerically(A<List<object>>.Ignored, A<List<string>>.Ignored, A<string>.Ignored, A<Type>.Ignored))
+                .Returns(callCreateNormalizedDataGenericallyReturn);
+
+            List<List<Animal>> denormalizedList = new List<List<Animal>>()
+            {
+                new List<Animal>() { new Animal() { Age=1, Name = "First"} },
+                new List<Animal>() { new Animal() { Age=2, Name = "Second"}, new Animal() { Age = 3, Name = "Third" } }
+            };
+            List<string> previousDataNames = new List<string>();
+            string dataName = "Astronomical";
+
+            List<List<object>> normalizedData = normalizedDataManager.CreateNormalizedDataForListOfIEnumerableType(denormalizedList, previousDataNames, dataName);
+
+            A.CallTo(() => normalizedDataManager.CallCreateNormalizedDataGenerically(A<List<object>>.Ignored, A<List<string>>.Ignored, A<string>.Ignored, A<Type>.Ignored))
+                .WhenArgumentsMatch(args =>
+                {
+                    return  ((List<object>)args[0])[0] == denormalizedList[0][0] &&
+                            ((List<object>)args[0])[1] == denormalizedList[1][0] &&
+                            ((List<object>)args[0])[2] == denormalizedList[1][1] &&
+                            args[1] == previousDataNames &&
+                            args[2] == null &&
+                            (Type) args[3] == typeof (Animal);
+                })
+                .MustHaveHappened(Repeated.Exactly.Once);
+        }
+
+        [Fact]
+        public void Should_Return_The_NormalizedData_Containing_The_Returned_List_From_CallCreateNormalizedDataGenerically()
+        {
+            List<List<object>> callCreateNormalizedDataGenericallyReturn = new List<List<object>>()
+            {
+                new List<object>(),
+                new List<object>()
+            };
+
+            A.CallTo(() => normalizedDataManager.CallCreateNormalizedDataGenerically(A<List<object>>.Ignored, A<List<string>>.Ignored, A<string>.Ignored, A<Type>.Ignored))
+                .Returns(callCreateNormalizedDataGenericallyReturn);
+
+            List<List<Animal>> denormalizedList = new List<List<Animal>>()
+            {
+                new List<Animal>() { new Animal() { Age=1, Name = "First"} },
+                new List<Animal>() { new Animal() { Age=2, Name = "Second"}, new Animal() { Age = 3, Name = "Third" } }
+            };
+            List<string> previousDataNames = new List<string>();
+            string dataName = "Astronomical";
+
+            List<List<object>> normalizedData = normalizedDataManager.CreateNormalizedDataForListOfIEnumerableType(denormalizedList, previousDataNames, dataName);
+            
+            Assert.Equal(dataName + "~", normalizedData[0][0]);
+            Assert.Equal(callCreateNormalizedDataGenericallyReturn[0], normalizedData[0][1]);
+            Assert.Equal(callCreateNormalizedDataGenericallyReturn[1], normalizedData[0][2]);
+        }
+    }
     #endregion
 
     #region CallCreateNormalizedDataGenerically
