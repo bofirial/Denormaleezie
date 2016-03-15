23802c720af69e227d6ce8839972b56337f20be7
James Schafer <james@jschafer.net>
2016-03-15 10:04:35 -0400

    Added a sample application using Normaleezie and comparing metrics w/ it and w/o it.

---
+++ test/Normaleezie.Sample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Normaleezie.Sample.MvcApplication" Language="C#" %>
---
+++ test/Normaleezie.Sample/Models/ZipCodeDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace Normaleezie.Sample.Models
+{
+    public class ZipCodeDTO
+    {
+        [JsonProperty("city")]
+        public string City { get; set; }
+
+        //[JsonProperty("loc")]
+        //public List<float> LatitudeLongitude { get; set; }
+
+        [JsonProperty("pop")]
+        public int Population { get; set; }
+
+        [JsonProperty("state")]
+        public string State { get; set; }
+
+        [JsonProperty("_id")]
+        public string ZipCode { get; set; }
+    }
+}
